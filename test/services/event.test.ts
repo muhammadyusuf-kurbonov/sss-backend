cdf40b9a65c90d95840b29ca9520d088e29d4047
muhammadyusuf <muhammadyusuf.kurbonov2002@gmail.com>
2021-12-25 00:07:46 +0500

    created tests for events

--- test/services/event.test.ts
+++ test/services/event.test.ts
@@ -1,10 +1,75 @@
 import assert from 'assert';
 import app from '../../src/app';
+import {describe} from 'mocha';
+import { notFound } from '@feathersjs/express';
 
 describe('\'event\' service', () => {
   it('registered the service', () => {
     const service = app.service('event');
 
     assert.ok(service, 'Registered the service');
+  });
+
+  it('create event', async ()=>{
+    const event = await app.service('event').create({
+      title: 'test-event',
+      description: 'Test event for testing',
+      maxScore: 100
+    });
+
+    assert.ok(event, 'Not created a event');
+    assert.ok(event._id, 'Event has not an id');
+    assert.strictEqual(event.title, 'test-event', 'Event title is incorrect');
+    assert.strictEqual(event.description, 'Test event for testing', 'Event description is incorrect');
+    assert.strictEqual(event.maxScore, 100, 'Event maxScore is incorrect');
+  });
+
+  it('get event', async ()=>{
+    const {_id} = await app.service('event').create({
+      title: 'test-event',
+      description: 'Test event for testing',
+      maxScore: 100
+    });
+
+    const event = await app.service('event').get(_id);
+    assert.ok(event, 'Not found a event');
+    assert.ok(event._id, 'Event has not an id');
+    assert.strictEqual(event.title, 'test-event', 'Event title is incorrect');
+    assert.strictEqual(event.description, 'Test event for testing', 'Event description is incorrect');
+    assert.strictEqual(event.maxScore, 100, 'Event maxScore is incorrect');
+  });
+
+  it('update event', async ()=>{
+    const {_id} = await app.service('event').create({
+      title: 'test-event',
+      description: 'Test event for testing',
+      maxScore: 100
+    });
+
+    const event = await app.service('event').patch(_id, {
+      title: 'test-event-updated',
+      description: 'Test event for testing updated',
+      maxScore: 200
+    });
+
+    assert.ok(event, 'Not updated a event');
+    assert.ok(event._id, 'Event has not an id');
+    assert.strictEqual(event.title, 'test-event-updated', 'Event title is incorrect');
+    assert.strictEqual(event.description, 'Test event for testing updated', 'Event description is incorrect');
+    assert.strictEqual(event.maxScore, 200, 'Event maxScore is incorrect');
+  });
+
+  it('delete event', async ()=>{
+    const {_id} = await app.service('event').create({
+      title: 'test-event',
+      description: 'Test event for testing',
+      maxScore: 100
+    });
+
+    const removed = await app.service('event').remove(_id);
+
+    assert.ok(removed, 'Not removed a event');
+    assert.ok(removed._id, 'Event has not an id');
+    assert.strictEqual(removed.title, 'test-event', 'Event title is incorrect');
   });
 });
