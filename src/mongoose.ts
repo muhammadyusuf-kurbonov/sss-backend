3380be7eda11754be86c9bf651c01beccc32df88
muhammadyusuf <muhammadyusuf.kurbonov2002@gmail.com>
2021-12-26 23:26:57 +0500

    deploy to qovery: password encoded

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -4,7 +4,8 @@ import logger from './logger';
 
 function buildMongoUri(app: Application): string {
   if (!process.env.QOVERY_APPLICATION_Z30302B0F_NAME) return app.get('mongodb');
-  return `mongodb://${process.env.QOVERY_MONGODB_ZF874FF2F_LOGIN}:${process.env.QOVERY_MONGODB_ZF874FF2F_PASSWORD}@${process.env.QOVERY_MONGODB_ZF874FF2F_HOST}:${process.env.QOVERY_MONGODB_ZF874FF2F_PORT}/yahoom`;
+  const pass = encodeURIComponent(process.env.QOVERY_MONGODB_ZF874FF2F_PASSWORD ?? '');
+  return `mongodb://${process.env.QOVERY_MONGODB_ZF874FF2F_LOGIN}:${pass}@${process.env.QOVERY_MONGODB_ZF874FF2F_HOST}:${process.env.QOVERY_MONGODB_ZF874FF2F_PORT}/yahoom`;
 }
 
 export default function (app: Application): void {
