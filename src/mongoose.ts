6d2645054d3c273cacdf1ef5ffb19a79c2365e5c
muhammadyusuf <muhammadyusuf.kurbonov2002@gmail.com>
2021-12-26 23:17:14 +0500

    deploy to qovery

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -2,11 +2,18 @@ import mongoose from 'mongoose';
 import { Application } from './declarations';
 import logger from './logger';
 
+function buildMongoUri(app: Application): string {
+  if (!process.env.QOVERY_APPLICATION_Z30302B0F_NAME) return app.get('mongodb');
+  return `mongodb://
+    ${process.env.QOVERY_MONGODB_ZF874FF2F_LOGIN}:${process.env.QOVERY_MONGODB_ZF874FF2F_PASSWORD}
+    @${process.env.QOVERY_MONGODB_ZF874FF2F_HOST_INTERNAL}
+    :${process.env.QOVERY_MONGODB_ZF874FF2F_PORT}/yahoom`;
+}
+
 export default function (app: Application): void {
+  console.log('Connecting to MongoDB...', buildMongoUri(app));
   mongoose.connect(
-    process.env.QOVERY_MONGODB_ZF874FF2F_DATABASE_URL_INTERNAL 
-      ? process.env.QOVERY_MONGODB_ZF874FF2F_DATABASE_URL_INTERNAL
-      : app.get('mongodb'),
+    buildMongoUri(app),
   ).catch(err => {
     logger.error(err);
     process.exit(1);
