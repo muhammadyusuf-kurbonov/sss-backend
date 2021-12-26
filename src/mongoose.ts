17f8cc355a1f43ebb4b24fab960a40ed93bb20c6
muhammadyusuf <muhammadyusuf.kurbonov2002@gmail.com>
2021-12-26 23:22:18 +0500

    deploy to qovery

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -4,10 +4,7 @@ import logger from './logger';
 
 function buildMongoUri(app: Application): string {
   if (!process.env.QOVERY_APPLICATION_Z30302B0F_NAME) return app.get('mongodb');
-  return `mongodb://
-    ${process.env.QOVERY_MONGODB_ZF874FF2F_LOGIN}:${process.env.QOVERY_MONGODB_ZF874FF2F_PASSWORD}
-    @${process.env.QOVERY_MONGODB_ZF874FF2F_HOST_INTERNAL}
-    :${process.env.QOVERY_MONGODB_ZF874FF2F_PORT}/yahoom`;
+  return `mongodb://${process.env.QOVERY_MONGODB_ZF874FF2F_LOGIN}:${process.env.QOVERY_MONGODB_ZF874FF2F_PASSWORD}@${process.env.QOVERY_MONGODB_ZF874FF2F_HOST}:${process.env.QOVERY_MONGODB_ZF874FF2F_PORT}/yahoom`;
 }
 
 export default function (app: Application): void {
